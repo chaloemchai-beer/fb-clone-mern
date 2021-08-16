c2305b804588efe17f3032642810ba758969b3fa
Chaloemchai Aintharaphithak <ChaleamchaiSt@gmail.com>
2021-08-16 22:27:28 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./Components/Header";
+import "./Styles/App.css";
+
+const App = () => {
+  return (
+    <div>
+      <div className="App">
+        <Header />
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
+    </div>
+  );
+};
+
+export default App;
